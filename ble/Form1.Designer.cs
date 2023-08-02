d5ab3940cf93243e4c7f9cdcc6392df4182d546a
feyzaozcini <77547055+feyzaozcini@users.noreply.github.com>
2023-08-02 14:21:21 +0300

    Add project files.

---
+++ ble/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace ble
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(21, 133);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(305, 207);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "DeviceName";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "DeviceID";
+            this.columnHeader2.Width = 200;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(21, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Scanning";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(192, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Stop Scanning";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(463, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 40);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Connect";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(665, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 40);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Disconnect";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(27, 383);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Status..";
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView2.FullRowSelect = true;
+            this.listView2.GridLines = true;
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(463, 133);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(305, 207);
+            this.listView2.TabIndex = 7;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Flag";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Value";
+            this.columnHeader4.Width = 200;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(460, 383);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Toplam Kesinti Sayısı : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 459);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ble/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
+
+namespace ble
+{
+    public partial class Form1 : Form
+    {
+
+        #region  Tanımlamalar
+
+        private BluetoothLEAdvertisementWatcher watcher = null;
+        private BluetoothLEDevice bluetoothLEDevice = null; 
+
+        public GattDeviceServicesResult result { get; set; }
+        public DeviceInformation device = null;
+
+        public GattCharacteristic selectedCharacteristic = null;
+        public GattDeviceService selectedService = null;
+
+        private static int notificationCounter;
+        private static DateTime lastValueChangedTime;
+
+        public string HEART_RATE_SERVICE_ID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
+        private bool connected = false;
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+
+        #region Ble Connection
+        
+        
+        private void Ble_startScanner()
+        {
+            listView1.Items.Clear();
+
+            watcher = new BluetoothLEAdvertisementWatcher();
+            
+            watcher.ScanningMode = BluetoothLEScanningMode.Active;
+            watcher.Received += OnAdcertisementReceived;
+
+            watcher.SignalStrengthFilter.OutOfRangeTimeout = TimeSpan.FromMilliseconds(1000);
+            watcher.SignalStrengthFilter.SamplingInterval = TimeSpan.FromMilliseconds(500);
+            watcher.Start();
+
+        }
+        string name_buffer = "";
+        string name_s = "";
+        string Deviceid = "";
+        private void OnAdcertisementReceived(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
+        {
+            name_buffer = args.Advertisement.LocalName;
+
+            if (name_buffer !=string.Empty)
+            {
+                name_s = name_buffer;
+                Deviceid = args.BluetoothAddress.ToString();
+
+                string[] bilgiler = { name_s, Deviceid };
+                ListViewItem lst = new ListViewItem(bilgiler);
+                listView1.Items.Add(lst);
+            }
+        }
+        
+        private void Ble_stopScanner()
+        {
+            watcher.Stop();
+            watcher.ScanningMode = BluetoothLEScanningMode.Passive;
+            watcher.Received -= OnAdcertisementReceived;
+        }
+
+        string name = "";
+        private async void Ble_Connection()
+        {
+            label1.Text = "Connecting..";
+
+            if (listView1.SelectedItems.Count>0)
+            {
+                name = listView1.SelectedItems[0].SubItems[0].Text;
+
+                string[] requestedProperties = { "System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected" , "System.Devices.Aep.Bluetooth.Le.IsConnectable" };
+                
+
+                DeviceWatcher deviceWatcher =
+                            DeviceInformation.CreateWatcher(
+                                    BluetoothLEDevice.GetDeviceSelectorFromPairingState(false),
+                                    requestedProperties,
+                                    DeviceInformationKind.AssociationEndpoint);
+
+
+                deviceWatcher.Added += DeviceWatcher_Added;
+                deviceWatcher.Updated += DeviceWatcher_Updated;
+                deviceWatcher.Removed += DeviceWatcher_Removed;
+
+                deviceWatcher.EnumerationCompleted += DeviceWatcher_EnumerationCompleted;
+                deviceWatcher.Stopped += DeviceWatcher_Stopped;
+
+                deviceWatcher.Start();
+
+                if (connected)
+                {
+                    return; // Zaten bağlantı kurulmuşsa bağlantıyı yeniden başlatma
+                }
+                connected = true;
+
+                while (connected)
+                {
+                    if (device==null)
+                    {
+                        Thread.Sleep(200);
+                    }
+                    else
+                    {
+
+                        bluetoothLEDevice = await BluetoothLEDevice.FromIdAsync(device.Id);
+                        result = await bluetoothLEDevice.GetGattServicesAsync();
+
+                        if (result.Status==GattCommunicationStatus.Success)
+                        {
+                           
+                            var services = result.Services;
+                            foreach (var service in services)
+                            {
+                                if (service.Uuid.ToString() == HEART_RATE_SERVICE_ID)
+                                {
+                                    label1.Text = "Connecting..";
+                                    GattCharacteristicsResult characteristicsResult = await service.GetCharacteristicsAsync();
+                                    if (characteristicsResult.Status==GattCommunicationStatus.Success)
+                                    {
+                                        label1.Text = "Connecting..";
+                                        var characteristics = characteristicsResult.Characteristics;
+                                        foreach (var characteristic in characteristics)
+                                        {
+                                            GattCharacteristicProperties properties = characteristic.CharacteristicProperties;
+
+                                            if (properties.HasFlag(GattCharacteristicProperties.Notify))
+                                            {
+                                                GattCommunicationStatus status = await characteristic.WriteClientCharacteristicConfigurationDescriptorAsync(GattClientCharacteristicConfigurationDescriptorValue.Notify);
+                                                if (status==GattCommunicationStatus.Success)
+                                                {
+                                                    label1.Text = "Connected..";
+                                                    characteristic.ValueChanged += Characteristic_ValueChanged;
+                                                    //bluetoothLEDevice.ConnectionStatusChanged += BluetoothLeDevice_ConnectionStatusChanged;
+                                                    selectedCharacteristic = characteristic;
+
+
+                                                    lastValueChangedTime = DateTime.Now;
+                                                    notificationCounter = 0;
+                                                    //label1.Text=$"{bluetoothLEDevice.ConnectionStatus}";
+                                                }
+                                                    
+                                             }
+
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        
+
+    /*private void BluetoothLeDevice_ConnectionStatusChanged(BluetoothLEDevice sender, object args)
+        {
+            throw new NotImplementedException();
+        }*/
+
+        private void Characteristic_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
+        {
+            var reader = DataReader.FromBuffer(args.CharacteristicValue);
+            var flags = reader.ReadByte();
+            var value = reader.ReadByte();
+
+            // flags ve value değerlerini listView2'ye ekleyelim
+            ListViewItem newItem = new ListViewItem(new string[] { flags.ToString(), value.ToString() });
+
+            // listView2'ye yeni öğeyi ekleyelim
+            listView2.Items.Add(newItem);
+            var currentTime = DateTime.Now;
+            var timeDiff = currentTime - lastValueChangedTime;
+            lastValueChangedTime = currentTime;
+
+            // Eğer zaman farkı 0.2 saniyeden büyükse, sinyal kesintisi olduğu kabul edilir
+            if (timeDiff.TotalSeconds > 0.2)
+            {
+                notificationCounter++;
+                //Console.WriteLine($"Sinyal kesintisi! Kesinti sayısı: {notificationCounter}");
+            }
+        }
+
+        private void DeviceWatcher_Stopped(DeviceWatcher sender, object args)
+        {
+            
+        }
+
+        private void DeviceWatcher_EnumerationCompleted(DeviceWatcher sender, object args)
+        {
+            
+        }
+
+        private void DeviceWatcher_Removed(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            
+        }
+
+        private void DeviceWatcher_Updated(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            
+        }
+
+        private void DeviceWatcher_Added(DeviceWatcher sender, DeviceInformation args)
+        {
+
+            if (args.Name == "FIZYOSOFT")
+            {
+                device = args;
+            }
+            
+            
+        }
+        
+        #endregion
+
+        #region Button Click Events
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Ble_startScanner();
+            label1.Text = "Scanning..";
+
+            // ListView2'yi temizleme
+            listView2.Items.Clear();
+            label2.Text = "Toplam Kesinti Sayısı : ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Ble_stopScanner();
+            label1.Text = "Scanning Stop..";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Ble_Connection();
+            //label1.Text = "Connected..";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            // Bağlantıyı durdurma ve gelen verileri temizleme
+            connected = false; // Döngüyü sonlandır
+
+            if (bluetoothLEDevice != null)
+            {
+                if (selectedCharacteristic != null)
+                {
+                    // Gelen veri olayı bağlantısını kaldırma
+                    selectedCharacteristic.ValueChanged -= Characteristic_ValueChanged;
+                }
+
+                // Cihazı kapatma
+                bluetoothLEDevice.Dispose();
+                bluetoothLEDevice = null;
+                selectedCharacteristic = null;
+            }
+            label1.Text = "Disconnected";
+            label2.Text= $"Toplam Kesinti Sayısı :  {notificationCounter}";
+           
+
+        }
+
+        #endregion
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+       
+    }
+}
